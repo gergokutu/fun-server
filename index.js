e09ea1aa489f15cd5c445bbba03c0109970fa243
gergokutu <gergo.kutu@gmail.com>
2019-09-06 15:47:11 +0200

    Arrow function in app.listen part

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => res.send('/get » Server is running'))
-app.listen(port, console.log(`Server listening on ${port}`))
+app.listen(port, () => console.log(`Server listening on ${port}`))
