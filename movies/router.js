8eac880580b9a1fd074dc8f1e5ea0c3fe161bb86
gergokutu <gergo.kutu@gmail.com>
2019-09-16 20:16:32 +0200

    Add movies to db

--- movies/router.js
+++ movies/router.js
@@ -5,18 +5,26 @@ const Movie = require('./model')
 
 const request = require('superagent')
 
-// I need movie posters for the memory cards through any API
-// check themoviedb.org first
 router.get(
   '/movies',
   async (req, res, next) => {
     try {
-    // const data = await axios.get(`${baseURL}/jobs?limit=100000`)
     const movies = await Movie.findAll()
     res.send(movies)
     } catch (error) {
       console.error(error)
     }
   })
+
+  // I need movie posters for the memory cards » db
+  // POST!!!
+
+router.post(
+  '/movies',
+  async (req, res, next) => {
+    const movie = await Movie.findOrCreate({ where: {title: req.body.title, posterUrl: req.body.posterUrl} })
+    res.send(movie)
+  }
+)
 
 module.exports = router
