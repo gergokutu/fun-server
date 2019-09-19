c8e42fcafb3b16969c3fab8f217378602a034bf8
gergokutu <gergo.kutu@gmail.com>
2019-09-19 12:06:39 +0200

    All the pages fetched into the database

--- movies/router.js
+++ movies/router.js
@@ -13,22 +13,24 @@ router.get(
     const size = '/w185'
 
     try {
-      const movies = await request.get('https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=377f16c90eeda4700f91c1925bbe3668')
-      const { results } = movies.body
-      
-      // how to get all the pages not only the first (of 500) - ?
-      // how to get the original_title as well from tmdb... - ok
-      // how to fill up Movie model » posterUrl from an array - ok
-      results.map(async (movie) => {
-        const movies = await Movie.create(
-          {
-            title: movie.original_title,
-            posterUrl: base_url.concat(size.concat(movie.poster_path))
-          }
-        )
-      })
-      
-      res.send(movies)
+      // how to get all the pages not only the first (of 500) - ok
+      for (let pageNumber = 1; pageNumber < 501; pageNumber++) {
+        const movies = await request.get(`https://api.themoviedb.org/3/discover/movie?page=${pageNumber}&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=377f16c90eeda4700f91c1925bbe3668`)
+        const { results } = movies.body
+        
+        // how to get the original_title as well from tmdb... - ok
+        // how to fill up Movie model » posterUrl from an array - ok
+        results.map(async (movie) => {
+          const movies = await Movie.create(
+            {
+              title: movie.original_title,
+              posterUrl: base_url.concat(size.concat(movie.poster_path))
+            }
+          )
+        })
+      }
+
+      res.send(results)
 
     } catch (error) {
       next(error)
